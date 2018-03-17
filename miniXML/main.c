84e1c29cceeaa248261822f6a4092cce571f03bb
Alassane <bonjour@alassanecoly.fr>
2018-03-17 01:45:24 +0100

    ✨ Added main.c with "Hello, World !"

---
+++ miniXML/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Mini XML
+//
+//  Created by Alassane Coly-Arogundade on 17/03/2018.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
